0c1237238e7099d914f4a73ef7f420fdc7018e5e
Sabrina Seitz <sabrina.seitz@gmail.com>
2020-09-20 12:49:38 +0200

    Troubleshot

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React from "react";
 import SearchEngine from "./SearchEngine";
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="container">
-      <h1>☔ Weather App</h1>
+      <h1>Weather App</h1>
       <SearchEngine />
     </div>
   );
 }
-
-export default App;
